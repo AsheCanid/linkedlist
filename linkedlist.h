2e51d2a1301b1eaf5aba28879afc5951520b3a00
Ashe <134902048+AsheCanid@users.noreply.github.com>
2023-06-02 00:10:10 -0500

    Update linkedlist.h
    Complete code refactor. Now compatible with c and uses struct instead of class for data storage.

--- linkedlist.h
+++ linkedlist.h
@@ -1,85 +1,80 @@
 #include <string>
 #include <iostream>
 
-class link;
 
-   
-
-class link
+struct link
 {
-    
-    
-    public:
     std::string data;
     link* next;
     link* previous;
-    
-    link(std::string d, link* n, link* p)
+}; 
+
+
+void editLink(link l, std::string data, link* n, link* p) // Adjust all data about the link
+{
+    l.data = data;
+    l.next = n;
+    l.previous = p;
+}
+
+void editLink(link l, std::string data) // Adjust only stored data
+{
+    l.data = data;
+}
+
+void linkadjust(link l, link* n, link* p) // Adjusts both the link to the previous and next link
+{
+    l.next = n;
+    l.previous = p;
+}
+
+void linkadjust(link l, link* n) // Adjusts only the pointer to the next link
+{
+    l.next = n;
+}
+
+void linkadjustprev(link l, link* p) // Adjusts the pointer to the link before this link
+{
+    l.previous = p;
+}
+
+void linkdelete(link l) // Deletes a link and adjusts the pointers of the next and previous link's pointers, use instead of free(link)
+{
+    if (l.previous != NULL && l.next != NULL) // A link before and a link after exist
     {
-        data = d;
-        next = n;
-        previous = p;
+        l.previous->next = l.next;
+        l.next->previous = l.previous;
+        free(&l);
+        std::cout << "Link Deleted Successfully";
     }
-    ~link()
+    else if (l.previous != NULL && l.next == NULL) // A link before exists, but no link after
+    {
+        l.previous->next = NULL;
+        free(&l);
+        std::cout << "Link Deleted Successfully";
+    }
+    else if (l.previous == NULL && l.next != NULL) // No link before, but a link after exists
+    {
+        l.next->previous = NULL;
+        free(&l);
+        std::cout << "Link Deleted Successfully";
+    }
+    else // Neither a link before or after exists
     {
         std::cout << "Link Deleted Successfully";
     }
-     void editor(std::string d, link* n, link* p)
-    {
-        data = d;
-        next = n;
-        previous = p;
-    }
-    
-    void linkDelete() // Call this fucntion when deleting a link to automatically restructure list
-    {
-        if (previous != nullptr && next != nullptr)
-        {
-            linkadjust(previous, next);
-            delete this;
-        }
-        else if (previous == nullptr && next != nullptr)
-        {
-            linkadjustp(*next, nullptr);
-            delete this;
-        }
-        else if (previous != nullptr && next == nullptr)
-        {
-            linkadjust(previous, nullptr);
-            delete this;
-        }
-        else
-        {
-            delete this;
-        }
-    }
-    
-    void linkadjust(link* p, link* n)
-    {
-        next = n;
-        previous= p;
-    }
-    void linkadjust( link* n)
-    {
-        next = n;
-    }
-    void linkadjustp(link ths, link* p)
-    {
-        ths.previous = p;
-    }
-};    
-
-
-void editor(link tbe, std::string d, link* next, link* previous)
-{
-    tbe.editor(d, next, previous);
 }
+
 void printLinks(link start)
+{
+    if (start.next != NULL)
     {
-    if (start.next != nullptr)
-        {
-            std::cout << start.data << std::endl;
-            printLinks(*(start.next));
-        }
+    std::cout << start.data << " - ";
+    printLinks(*start.next);
+    }
+    else
+    {
+        std::cout << start.data << "\n Print complete";
     }
 
+}
