97565b280dcd8422b2d376568f146b50c7002880
Ashe <134902048+AsheCanid@users.noreply.github.com>
2023-06-22 23:14:29 -0500

    Update and rename linkedlist.h to linkedlist.cpp
    Complete restructuring and new functions/error handling for the file. Changed from .h to .cpp since they operate in #include the same way but fixes bugs a bit.

---
+++ linkedlist.cpp
@@ -0,0 +1,115 @@
+#include <string>
+#include <iostream>
+
+
+class link
+{
+   
+    private:
+		link * head; // First link in list
+		link * previous; // Link before the current link (if none, nullptr)
+		link * next; // Link after this (if none, nullptr)
+		link * tail; // Last link in list
+		int data;
+	public:
+    link() // Create link with no data value
+    {
+	next = nullptr;
+	previous = nullptr;	
+	}
+	link(int val) // Create link with value
+	{
+		next = nullptr;
+		previous = nullptr;	
+		data = val;
+	}
+	link(int val, link * prev) // Create link with previous link provided
+	{
+		data = val;
+		previous = prev;
+    }
+	link(int val, link* prev, link * nxt) // Create link with previous and next link provided
+	{
+		data = val;
+		previous = prev;
+		next = nxt;
+	}
+	void createLink(int data) // Creates linnks dynamically
+	{
+		link* temp = new link;
+		temp->data = data;
+		temp->next = nullptr;
+		if (head == nullptr)
+        {
+            head = temp;
+            tail = temp;
+        }
+        else
+        {   
+            tail->next = temp;
+            tail = temp;
+        }	
+	}
+	void printLinks() // Prints values in list from lowest to highest index
+	{
+		link * temp = head;
+		while (temp != nullptr)
+		{
+			std::cout << temp->data << std::endl;
+            temp = temp->next;
+		}
+	}
+	void newStartLink(int val) // Creates a new start/head link
+	{
+		link * newLink;
+		newLink = new link;
+		newLink->data = val;
+		newLink->next = head;
+		head = newLink;
+
+	}
+	void insertLink(int pos, int val) // Insert a link at any given position, save beginning of the list
+	{
+		link * prev;
+		link * temp = new link(val);
+		link * thisLink = head;
+		prev = head;
+		if (pos <= 0)
+		throw std::invalid_argument("Invalid Input, Must be greater than 0"); // A pos of <= 0 causes an endless loop
+		for (int i = 0; i < pos; i++)
+		{
+			prev = thisLink;
+			try
+			{thisLink = thisLink->next;}
+			catch(...)
+			{
+				thisLink = prev;
+				thisLink->next = nullptr;
+			}
+		}
+		prev->next = temp; 
+		temp->next = thisLink;
+	}
+	void newEndLink(int val) // Insert link at the end of the list
+	{
+		link * newLink;
+		newLink = new link;
+		newLink->previous = tail;
+		newLink->data = val;
+		tail = newLink;
+	}
+	int deleteLink(int pos)
+	{
+		link * temp;
+		if (pos == 0)
+		delete head;
+		for (int i = 0; i < pos; i++)
+		{
+			temp = temp->next;
+		}
+		delete temp;
+	}
+	
+	
+}; 
+
